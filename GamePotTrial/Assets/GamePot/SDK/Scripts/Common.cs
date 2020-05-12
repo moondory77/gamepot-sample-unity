6df0b7b6750552785a315d1dada6f1aea4678d4e
moondory77 <moondory777@gmail.com>
2020-05-12 13:45:27 +0900

    cleanup usless resource

---
+++ GamePotTrial/Assets/GamePot/SDK/Scripts/Common.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NCommon
+{
+
+	// openDeep MenuID
+	// public enum AdActions
+	// {
+	//     APPLICATION_START,
+	//     RESUME,
+	//     PAUSE,
+	//     TUTORIAL_COMPLETE,
+	//     LEVEL,
+	//     EVENT,
+	//     BILLING
+	// }
+
+	public enum LoginType
+	{
+		NONE,
+		GOOGLE,
+		GOOGLEPLAY,
+		FACEBOOK,
+		NAVER,
+		GAMECENTER,
+		TWITTER,
+		LINE,
+		APPLE,
+		GUEST,
+		THIRDPARTYSDK
+	}
+
+	public enum ChannelType
+	{
+		GOOGLEPLAY,
+		GOOGLE,
+		FACEBOOK,
+		NAVER,
+		GAMECENTER,
+		TWITTER,
+		LINE,
+		APPLE,
+		THIRDPARTYSDK
+	}
+
+	// public enum AdType
+	// {
+	//     FACEBOOK,
+	//     ADJUST,
+	//     ADBRIX,
+	// }
+
+	public enum LinkingType
+	{
+		GOOGLEPLAY,
+		GAMECENTER,
+		GOOGLE,
+		FACEBOOK,
+		NAVER,
+		TWITTER,
+		LINE,
+		APPLE,
+		THIRDPARTYSDK
+	}
+
+	public enum GameOrientation
+	{
+		portrait = 1,
+		landscape = 2,
+	}
+
+	public enum GameLanguage
+	{
+		KOREAN = 0,
+		ENGLISH = 1,
+		CHINESE_CN = 2,
+		CHINESE_TW = 3,
+		GERMAN = 4,
+		JAPANESE = 5
+	}
+
+
+	// -------- RESPONSE RESULT STATE --------
+	public enum ResultLogin
+	{
+		SUCCESS,
+		CANCELLED,
+		FAILED,
+		NEED_UPDATE,
+		MAINTENANCE,
+		APP_CLOSE
+	}
+
+	public enum ResultPurchase
+	{
+		SUCCESS,
+		CANCELLED,
+		FAILED
+	}
+
+	public enum ResultLinking
+	{
+		SUCCESS,
+		CANCELLED,
+		FAILED
+	}
+}
+
